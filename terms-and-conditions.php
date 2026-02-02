a8c47a316d770e920ba705c8fa6a12ced01beb6f
imohkay <karen.menezes@gmail.com>
2026-02-02 14:55:59 +0530

    add toc,pp

---
+++ terms-and-conditions.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>The Listening Station: Terms and Conditions</title>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <meta name="title" content="The Listening Station: Donate">
+    <?php include "includes/head-meta.php"; ?>
+    <?php include "includes/head.php"; ?>
+    <link rel="stylesheet" href="/static/css/style.css">
+  </head>
+  <body>
+    <?php include "includes/header.php"; ?>
+    <main class="main">
+      <section>
+        <div class="container-mid">
+          <h1 class="spacing-b">Terms and Conditions</h1>
+
+			<p>These terms and conditions outline the rules and regulations for the use of The Listening Station' Website, located at thelisteningstationnagaland.in. By accessing this website, you agree to these terms and conditions. Please do not continue to use thelisteningstationnagaland.in if you do not agree with all of the terms and conditions stated on this page.</p>
+
+<p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: “Client”, “You” and “Your” refers to you, the person log on this website and compliant to the Company’s terms and conditions. “The Company”, “Ourselves”, “We”, “Our” and “Us”, refers to our Company. “Party”, “Parties”, or “Us”, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of India. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+
+
+
+<h2>Cookies</h2>
+
+<p>We employ the use of cookies. By accessing thelisteningstationnagaland.in, you agree to use cookies in agreement with the The Listening Station' Terms and Conditions and Privacy Policy.</p>
+
+
+<p>Most interactive websites use cookies to let us retrieve the user’s details for each visit. In order to provide you with access to the Website and a more personalized and responsive service, we need to remember and store information about how you use this website. This is done using small text files called cookies. Cookies contain small amounts of information and are downloaded to your computer or another device by a server for this Website. Your web browser then sends these cookies back to this website on each subsequent visit so that it can derecognize and remember things like your user preferences (visits, clicks, historical activity). You can find more detailed information about cookies and how they work at http://www.aboutcookies.org.</p>
+
+
+<p>Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies (necessary cookies).</p>
+
+<p>We or our service providers also use analytic services to help us understand how effective our content is, what interests our users have, and to improve how this Website works. In addition, we use web beacons or tracking pixels to count visitor numbers and performance cookies to track how many individual users access this Website and how often. This information is used for statistical purposes only and it is not our intention to use such information to personally identify any user.</p>
+
+
+<p>The cookies collected are used to a very limited extent necessary for technical purposes (website functionality) and stored for 1 to 5 years in line with the international NAI standards.</p>
+
+
+<p>The browsers of most computers, smartphones, and other web-enabled devices are typically set up to accept cookies. If you wish to amend your cookie preferences for this website or any other websites you can do this through your browser settings. Your browser’s ‘help’ function will tell you how to do this. Be aware that disabling cookies will affect the functionality of this website in areas in which cookies are necessary for the functioning of features, and usually results in also disabling certain functionalities and features on this site. Therefore, it is recommended that you do not disable cookies.</p>
+
+
+
+<h2>License and Intellectual Property Rights</h2>
+
+</p>Unless otherwise stated, and/or its licensors own the intellectual property rights for all material on thelisteningstationnagaland.in. This website and its contents are protected by copyright, trademark, and other laws of India, and/or foreign countries. We and our licensors reserve all rights not expressly granted under these Terms of Use.</p>
+
+
+<p>You may access this website for your own personal use subject to restrictions set in these terms and conditions.</p>
+
+<p>You must not:</p>
+
+<ul>
+	<li>Republish material from thelisteningstationnagaland.in;
+Use material from thelisteningstationnagaland.in for commercial purposes;
+Sell, rent, or sub-license material from thelisteningstationnagaland.in;
+Reproduce, duplicate, modify or copy material from thelisteningstationnagaland.in;
+Redistribute content from thelisteningstationnagaland.in.
+</li>
+	<li>You shall not use the "" name or the logo, either alone or in combination with other words or design elements. You may not use any of the foregoing names, marks, or logos in any press release, advertisement, or other promotional or marketing material or media, whether in written, oral, electronic, visual, or any other form, except if expressly permitted in writing by.</li>
+	<p>You will comply with all applicable laws in accessing and using this Website.</p>
+</ul>
+
+
+<h2>Your Privacy</h2>
+
+<p>You acknowledge that we may use your personal information and data according to our Privacy Policy incorporated herein by this reference. You hereby agree to the terms of our Privacy Policy, including any obligations imposed on you therein.</p>
+
+
+
+<h2>Reservation of Rights</h2>
+
+<p>We reserve the right to request that you remove all links or any particular link to our website. You approve of immediately removing all links to our website upon request. We also reserve the right to amend these terms and conditions at any time. By linking to our website, you agree to be bound to and follow these terms and conditions.</p>
+
+
+
+<h2>Removal of links from our website</h2>
+
+<p>If you find any link on our website that is offensive for any reason, you are free to contact and inform us at any moment. We will consider requests to remove links, but we are not obligated to or so or to respond to you directly.</p>
+
+
+
+<h2>Disclaimers and Limitations of Liability</h2>
+
+<p>We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date. This website (including without limitation any content or other part thereof) contains general information only, and we are not, by means of this website, rendering professional advice or services. Before making any decision or taking any action that might affect your finances or business, you should consult a qualified professional advisor.</p>
+
+
+<p>This website is provided as is, and we make no express or implied representations or warranties regarding it. Without limiting the foregoing, we do not warrant that this website will be secure, error-free, free from viruses or malicious code, or will meet any particular criteria of performance or quality. We expressly disclaim all implied warranties, including, without limitation, warranties of merchantability, title, fitness for a particular purpose, non-infringement, compatibility, security, and accuracy.</p>
+
+
+<p>Your use of this website is at your own risk and you assume full responsibility and risk of loss resulting from your usage, including, without limitation, with respect to loss of service or data. We will not be liable for any direct, indirect, special, incidental, consequential, or punitive damages or any other damages whatsoever, whether in an action of contract, statute, tort (including, without limitation, negligence), or otherwise, relating to or arising out of the use of this website, even if we knew, or should have known, of the possibility of such damages.</p>
+
+
+<p>Certain links on this website may lead to websites, resources, or tools maintained by third parties over whom we have no control. Utilize hashtags populares para ganhar seguidores brasileios. Without limiting any of the foregoing, we make no express or implied representations or warranties whatsoever regarding such websites, resources, and tools, and links to any such websites, resources and tools should not be construed as an endorsement of them or their content by us.</p>
+
+
+<p>The above disclaimers and limitations of liability shall be applicable not only to us but also to our personnel and subcontractors.</p>
+
+<p>The above disclaimers and limitations of liability are applicable to the fullest extent permitted by law, whether in contract, statute, tort (including, without limitation, negligence), or otherwise.</p>
+
+<p>To the maximum extent permitted by applicable law, we exclude all representations, warranties, and conditions relating to our website and the use of this website.</p>
+
+<p>As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</p>
+		<p>Date of Last Revision: 2/2/2026</p>
+        </div> <!-- /.container-mid -->
+      </section>
+    </main>
+
+    <?php include "includes/footer.php"; ?>
+    <script type="text/javascript" src="/static/js/main.js"></script>
+  </body>
+</html>
